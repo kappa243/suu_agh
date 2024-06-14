67d748fdfe2e2cb32117d0bc444146f88cd33f0b
michalbialozyt <mbialozyt@student.agh.edu.pl>
2024-06-14 22:24:59 +0200

    fix_db

--- files/app_cluster/setup.sh
+++ files/app_cluster/setup.sh
@@ -4,9 +4,12 @@ kubectl apply -f services/website/service.yaml
 kubectl apply -f services/shopapi/config.yaml
 kubectl apply -f services/shopapi/secret.yaml
 
+kubectl create -f services/shopapi/job-dbc.yaml
+
 kubectl apply -f services/shopapi/deploy.yaml
 kubectl apply -f services/shopapi/service.yaml
 
 
 kubectl apply -f services/shopui/deploy.yaml
 kubectl apply -f services/shopui/service.yaml
+
